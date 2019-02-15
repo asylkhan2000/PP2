a851960543e98b84e97f511f38ee70400b22ce8d
asylkhan2000 <45388069+asylkhan2000@users.noreply.github.com>
2019-02-16 01:07:46 +0600

    Update Program.cs

--- week2/task2/task2/Program.cs
+++ week2/task2/task2/Program.cs
@@ -9,7 +9,7 @@ namespace task2
 {
     class Program
     {
-        static bool Prime(int a)
+        static bool Prime(int a)//создаем функцию, которая принимает целое число и возвращает true, если число простое или false, если нет
         {
             if (a == 1)
             {
@@ -26,15 +26,15 @@ namespace task2
         }
         static void Main(string[] args)
         {
-            StreamReader input = new StreamReader(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task2\input.txt");
+            StreamReader input = new StreamReader(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task2\input.txt");//считываем текст из файла input.txt
             StreamWriter output = new StreamWriter(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task2\output.txt");
-            string s = input.ReadToEnd();
-            string[] arr = s.Split(' ');
-            foreach (string n in arr)
+            string s = input.ReadToEnd();//сохраняем весь текст из файла в переменную s
+            string[] arr = s.Split(' ');//сохраняем в массив строки, отделенные пробелом в строке s
+            foreach (string n in arr)//пробегаемся по всем элементам массива
             {
-                if (Prime(int.Parse(n)))
+                if (Prime(int.Parse(n)))//обращаемся к функции Prime, переводя элемент массива из строки в целое число
                 {
-                    output.Write(n + " ");
+                    output.Write(n + " ");//вывод простых чисел в файл output через пробел
                 }
             }
             input.Close();
