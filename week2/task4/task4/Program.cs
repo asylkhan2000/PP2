7e8c379adf2287cd81dbacb7d5b08d523877990a
asylkhan2000 <45388069+asylkhan2000@users.noreply.github.com>
2019-02-16 02:20:29 +0600

    Update Program.cs

--- week2/task4/task4/Program.cs
+++ week2/task4/task4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +12,11 @@ namespace task4
         static void Main(string[] args)
         {
             StreamWriter writer = new StreamWriter(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\first.txt");
-            string s = Console.ReadLine();
-            writer.Write(s);
+            string s = Console.ReadLine();//переменную заполняем текстом через консоль
+            writer.Write(s);//заполняем файл текстом
             writer.Close();
-            File.Copy(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\first.txt", @"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\second.txt");
-            File.Delete(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\first.txt");
+            File.Copy(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\first.txt", @"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\second.txt");//копируем файл в другой файл
+            File.Delete(@"C:\Users\FORA-TRADE\Desktop\PP2\week2\task4\first.txt");//удаляем первый файл
         }
     }
 }
